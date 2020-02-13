3156d46a10d84ef51411b1922ac4cc13a81fb273
Roman Matviy <roman@matviy.pp.ua>
2020-02-13 15:11:58 +0200

    fix error

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
   $("#mySlider").slick({
-    // autoplay: true,
+    autoplay: true,
     dots: false,
     arrows: true,
+    asNavFor: "#mySlider2",
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $("#mySlider2").slick({
+    // autoplay: true,
+    dots: false,
+    arrows: false,
+    asNavFor: "#mySlider",
     responsive: [
       {
         breakpoint: 990,
